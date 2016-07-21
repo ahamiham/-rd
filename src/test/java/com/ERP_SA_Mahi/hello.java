787a8eb6a0098cddc5179825df30eccf85dba139
mahesh.t <mahesh.t@qedge.com>
2016-07-21 15:27:55 +0530

    3rdone

---
+++ src/test/java/com/ERP_SA_Mahi/hello.java
@@ -0,0 +1,13 @@
+package com.ERP_SA_Mahi;
+
+public class hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello");
+	}
+
+}
